7f03d58b47a5b8d711b93b04946657cf64d7e0d0
Andres Forero <afforeroc@unal.edu.co>
2018-12-25 23:07:49 -0600

    Rectagular algorithm was improved

--- primality_test_basic_algorithm.cpp
+++ primality_test_basic_algorithm.cpp
@@ -1,53 +1,46 @@
-#include <math.h>       // sqrt, floor
-#include <vector>   	// vector
-#include <fstream>		// ofstream
-#include <iostream> 	// cout
-#include <vector>   	// vector
-#include <string.h> 	// memset
-#include <time.h>		// clock, clock_t
-#include <stdio.h>		// printf
-#include <cmath>		// log10
-#include <fstream>		// ofstream
+#include <math.h>   // sqrt, floor, log10
+#include <vector>   // vector
+#include <iostream> // cout
+#include <fstream>  // ofstream
 using namespace std;
 
 // Functions
+void printTitle(string title);
 bool isPrime(int n);
 vector<int> testPrimalityBasic(int limit);
 void primes2txt(vector<int> vector_primes);
 void primes2terminal(vector<int> vector_primes);
 
 int main(){
-    cout << "-----------------------------------" << endl;
-	cout << "PRIMALITY TEST with basic algorithm" << endl;
-	cout << "-----------------------------------" << endl;
-    int limit = 1e6;
+	printTitle("PRIMALITY TEST with basic algorithm");
+    int limit = 1e7;
+    cout << "The limit is 1e" << log10(limit) << endl;
     vector<int> primes = testPrimalityBasic(limit);
     int siz = primes.size();
     cout << "Number of primes obtained is " << siz << endl;
     cout << "Last prime number obtained is " << primes.at(siz-1) << endl;
-    //random_shuffle(vector_primes.begin(), vector_primes.end());
-    //primes2txt(primes);
     return 0;
+}
+
+void printTitle(string title){
+    int siz = title.size();
+    for(int i=0; i<siz; i++){ cout << "-"; } cout << endl;
+    cout << title << endl;
+    for(int i=0; i<siz; i++){ cout << "-"; } cout << endl;
 }
 
 bool isPrime(int n){
     bool resp = true;
     int a = floor(sqrt(n));
     for(int i=3; i<=a; i+=2){
-        if(n%i==0){
-            resp = false;
-            break;
-        }
+        if(n%i == 0){ return false; }
     }
 }
 
 vector<int> testPrimalityBasic(int limit){
-    vector<int> primes;
-    primes.push_back(2);
+    vector<int> primes; primes.push_back(2);
     for(int n=3; n<limit; n+=2){
-        if(isPrime(n)){
-            primes.push_back(n);
-        }
+        if(isPrime(n)){ primes.push_back(n); }
     }
     return primes;
 }
--- primality_test_rectangular_algorithm.cpp
+++ primality_test_rectangular_algorithm.cpp
@@ -1,56 +1,50 @@
-#include <math.h>       // sqrt, floor
-#include <vector>   	// vector
-#include <fstream>		// ofstream
-#include <iostream> 	// cout
-#include <vector>   	// vector
-#include <string.h> 	// memset
-#include <time.h>		// clock, clock_t
-#include <stdio.h>		// printf
-#include <cmath>		// log10
-#include <fstream>		// ofstream
-#include <regex>		// regex
+#include <math.h>   // sqrt, floor, log10
+#include <vector>   // vector
+#include <iostream> // cout
+#include <fstream>  // ofstream
 using namespace std;
 
 // Functions
+void printTitle(string title);
 bool isPrimeR(int n);
 vector<int> testPrimalityR(int limit);
 void primes2txt(vector<int> vector_primes);
 void primes2terminal(vector<int> vector_primes);
 
 int main(){
-    cout << "-----------------------------------------" << endl;
-	cout << "PRIMALITY TEST with rectangular algorithm" << endl;
-	cout << "-----------------------------------------" << endl;
-	int limit = 1e6;
+    printTitle("PRIMALITY TEST with rectangular algorithm");
+    int limit = 1e7;
+    cout << "The limit is 1e" << log10(limit) << endl;
     vector<int> primes = testPrimalityR(limit);
     int siz = primes.size();
     cout << "Number of primes obtained is " << siz << endl;
     cout << "Last prime number obtained is " << primes.at(siz-1) << endl;
-    //random_shuffle(vector_primes.begin(), vector_primes.end());
-    //primes2terminal(primes);
     primes2txt(primes);
     return 0;
+}
+
+void printTitle(string title){
+    int siz = title.size();
+    for(int i=0; i<siz; i++){ cout << "-"; } cout << endl;
+    cout << title << endl;
+    for(int i=0; i<siz; i++){ cout << "-"; } cout << endl;
 }
 
 bool isPrimeR(int n){
     bool resp = true;
     int a = floor(sqrt(n)); //raiz inicial
     int h;
-    while(a>2){
-        h = n-a*a; //residuo
-        if (h%a == 0){ return false; }
-        a--;
+    for(int i=3; i<=a; i+=2){
+        h = n-i*i; //residuo
+        if (h%i == 0){ return false; }
     }
     return resp;
 }
 
 vector<int> testPrimalityR(int limit){
-    vector<int> primes;
-    primes.push_back(2);
+    vector<int> primes; primes.push_back(2);
     for(int n=3; n<limit; n+=2){
-        if(isPrimeR(n)){
-            primes.push_back(n);
-        }
+        if(isPrimeR(n)){ primes.push_back(n); }
     }
     return primes;
 }
