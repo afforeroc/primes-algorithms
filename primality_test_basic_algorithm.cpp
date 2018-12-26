1691310ecaba83ec4d2647e909a3acb2a4deb7fd
Andres Forero <afforeroc@unal.edu.co>
2018-12-25 23:16:20 -0600

    Some minor changes on basic algorithm

--- primality_test_basic_algorithm.cpp
+++ primality_test_basic_algorithm.cpp
@@ -12,7 +12,7 @@ void primes2txt(vector<int> vector_primes);
 void primes2terminal(vector<int> vector_primes);
 
 int main(){
-	printTitle("PRIMALITY TEST with basic algorithm");
+    printTitle("PRIMALITY TEST with basic algorithm");
     int limit = 1e7;
     cout << "The limit is 1e" << log10(limit) << endl;
     vector<int> primes = testPrimalityBasic(limit);
@@ -20,7 +20,7 @@ int main(){
     cout << "Number of primes obtained is " << siz << endl;
     cout << "Last prime number obtained is " << primes.at(siz-1) << endl;
     return 0;
-}
+    }
 
 void printTitle(string title){
     int siz = title.size();
